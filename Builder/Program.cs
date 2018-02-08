1418be87304d16d2b6dd31782da22ab6bec8eef4
samir.musa92 <samir.musa92@gmail.com>
2018-02-08 16:39:43 +0400

    08.02.2018, 16:39

---
+++ Builder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VehicleBuilder builder;
+
+            Shop shop = new Shop();
+
+            builder = new ScooterBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            builder = new CarBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            builder = new MotorCycleBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+        }
+    }
+}
---
+++ Builder/VehicleBuilder.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    abstract class VehicleBuilder
+    {
+        protected Vehicle vehicle;
+
+        public Vehicle Vehicle
+        {
+            get { return vehicle; }
+        }
+
+        public abstract void BuildFrame();
+        public abstract void BuildEngine();
+        public abstract void BuildWheels();
+        public abstract void BuildDoors();
+    }
+
+    class MotorCycleBuilder : VehicleBuilder
+    {
+        public MotorCycleBuilder()
+        {
+            vehicle = new Vehicle("MotorCycle");
+        }
+
+        public override void BuildFrame()
+        {
+            vehicle["frame"] = "MotorCycle Frame";
+        }
+
+        public override void BuildEngine()
+        {
+            vehicle["engine"] = "500 cc";
+        }
+
+        public override void BuildWheels()
+        {
+            vehicle["wheels"] = "2";
+        }
+
+        public override void BuildDoors()
+        {
+            vehicle["doors"] = "0";
+        }
+    }
+
+    class CarBuilder : VehicleBuilder
+    {
+        public CarBuilder()
+        {
+            vehicle = new Vehicle("Car");
+        }
+
+        public override void BuildFrame()
+        {
+            vehicle["frame"] = "Car Frame";
+        }
+
+        public override void BuildEngine()
+        {
+            vehicle["engine"] = "2500 cc";
+        }
+
+        public override void BuildWheels()
+        {
+            vehicle["wheels"] = "4";
+        }
+
+        public override void BuildDoors()
+        {
+            vehicle["doors"] = "4";
+        }
+    }
+
+    class ScooterBuilder : VehicleBuilder
+    {
+        public ScooterBuilder()
+        {
+            vehicle = new Vehicle("Scooter");
+        }
+
+        public override void BuildFrame()
+        {
+            vehicle["frame"] = "Scooter Frame";
+        }
+
+        public override void BuildEngine()
+        {
+            vehicle["engine"] = "50 cc";
+        }
+
+        public override void BuildWheels()
+        {
+            vehicle["wheels"] = "2";
+        }
+
+        public override void BuildDoors()
+        {
+            vehicle["doors"] = "0";
+        }
+    }
+}
