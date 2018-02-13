61053df470089dc092672797f45cfd6d6f6adfc0
samir.musa92 <samir.musa92@gmail.com>
2018-02-13 21:48:43 +0400

    13.02.2018, 21:48

---
+++ FactoryMethod/Figura.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    abstract class Figure
+    {
+        public abstract int[,] Arr { get; set; }
+        public abstract void draw(int[,] Arr);
+    }
+
+    class One : Figure
+    {
+        public One()
+        {
+            Arr = new int[4, 4]
+            {
+                { 1, 1, 1, 0 },
+                { 1, 0, 0, 0 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 }
+            };
+
+            draw(Arr);
+        }
+
+        public override int[,] Arr { get{ return Arr; } set { Arr = value; } }
+
+        public override void draw(int[,] Arr)
+        {
+            for (int i = 0; i < Arr.Length / 4; i++)
+            {
+                for (int j = 0; j < Arr.Length / 4; j++)
+                {
+                    Console.Write(Arr[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Two : Figure
+    {
+        public Two()
+        {
+            Arr = new int[4, 4]
+            {
+                { 0, 1, 1, 0 },
+                { 1, 1, 0, 0 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 }
+            };
+
+            draw(Arr);
+        }
+
+        public override int[,] Arr { get { return Arr; } set { Arr = value; } }
+
+        public override void draw(int[,] Arr)
+        {
+            for (int i = 0; i < Arr.Length / 4; i++)
+            {
+                for (int j = 0; j < Arr.Length / 4; j++)
+                {
+                    Console.Write(Arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Three : Figure
+    {
+        public Three()
+        {
+            Arr = new int[4, 4]
+            {
+                { 1, 1, 0, 0 },
+                { 0, 1, 1, 0 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 }
+            };
+
+            draw(Arr);
+        }
+
+        public override int[,] Arr { get { return Arr; } set { Arr = value; } }
+
+        public override void draw(int[,] Arr)
+        {
+            for (int i = 0; i < Arr.Length / 4; i++)
+            {
+                for (int j = 0; j < Arr.Length / 4; j++)
+                {
+                    Console.Write(Arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Four : Figure
+    {
+        public Four()
+        {
+            Arr = new int[4, 4]
+            {
+                { 0, 1, 0, 0 },
+                { 1, 1, 1, 0 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 }
+            };
+
+            draw(Arr);
+        }
+
+        public override int[,] Arr { get { return Arr; } set { Arr = value; } }
+
+        public override void draw(int[,] Arr)
+        {
+            for (int i = 0; i < Arr.Length / 4; i++)
+            {
+                for (int j = 0; j < Arr.Length / 4; j++)
+                {
+                    Console.Write(Arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Five : Figure
+    {
+        public Five()
+        {
+            Arr = new int[4, 4]
+            {
+                { 1, 0, 0, 0 },
+                { 1, 1, 1, 0 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 }
+            };
+
+            draw(Arr);
+        }
+
+        public override int[,] Arr { get { return Arr; } set { Arr = value; } }
+
+        public override void draw(int[,] Arr)
+        {
+            for (int i = 0; i < Arr.Length / 4; i++)
+            {
+                for (int j = 0; j < Arr.Length / 4; j++)
+                {
+                    Console.Write(Arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Six : Figure
+    {
+        public Six()
+        {
+            Arr = new int[4, 4]
+            {
+                { 0, 1, 0, 0 },
+                { 0, 1, 0, 0 },
+                { 0, 1, 0, 0 },
+                { 0, 1, 0, 0 }
+            };
+
+            draw(Arr);
+        }
+
+        public override int[,] Arr { get { return Arr; } set { Arr = value; } }
+
+        public override void draw(int[,] Arr)
+        {
+            for (int i = 0; i < Arr.Length / 4; i++)
+            {
+                for (int j = 0; j < Arr.Length / 4; j++)
+                {
+                    Console.Write(Arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ FactoryMethod/FigureFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class FigureFactory
+    {
+        public static Figure GetFigure(string figure)
+        {
+            switch(figure)
+            {
+                case "One": return new One();
+                case "Two": return new Two();
+                case "Three": return new Three();
+                case "Four": return new Four();
+                case "Five": return new Five();
+                case "Six": return new Six();
+                default: throw new Exception($"Invalidate figure: {figure}");
+            }
+        }
+    }
+}
