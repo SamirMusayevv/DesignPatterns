d3f98ac0971c7185d153d4c6342db69bc994e03e
samir.musa92 <samir.musa92@gmail.com>
2018-03-05 01:02:04 +0400

    05.03.2018

---
+++ Memento/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memento
+{
+    public partial class Form1 : Form
+    {
+        private TextBoxHistory history { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            history = new TextBoxHistory();
+            history.Add(new Memento(richTextBox1.Text));
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            history.Add(new Memento(richTextBox1.Text));
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            richTextBox1.TextChanged -= richTextBox1_TextChanged;
+            try
+            {
+                richTextBox1.Text = history.Undo().Buffer;
+            }
+            catch (Exception)
+            {
+
+            }
+            finally { richTextBox1.TextChanged += richTextBox1_TextChanged; }
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            richTextBox1.TextChanged -= richTextBox1_TextChanged;
+            try
+            {
+                richTextBox1.Text = history.Redo().Buffer;
+            }
+            catch (Exception)
+            {
+            }
+            finally { richTextBox1.TextChanged += richTextBox1_TextChanged; }
+        }
+    }
+}
---
+++ Memento/Memento.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    class Memento
+    {
+        public string Buffer { get; set; }
+
+        public Memento(string Buffer)
+        {
+            this.Buffer = Buffer.Clone().ToString();
+        }
+    }
+
+    class TextBoxHistory
+    {
+        private List<Memento> Storage { get; set; }
+        private int _currenIndex;
+
+        public TextBoxHistory()
+        {
+            Storage = new List<Memento>();
+            _currenIndex = -1;
+        }
+
+        public void Add(Memento buffer)
+        {
+            if (_currenIndex == Storage.Count - 1)
+                Storage.Add(buffer);
+            else
+            {
+                Storage.RemoveRange(_currenIndex + 1, Storage.Count - _currenIndex - 1);
+                Storage.Add(buffer);
+            }
+            _currenIndex++;
+        }
+
+        public Memento Undo()
+        {
+            if (_currenIndex == 0) return Storage[_currenIndex];
+            else
+                return Storage[--_currenIndex];
+        }
+
+        public Memento Redo()
+        {
+            if (_currenIndex == Storage.Count - 1) return Storage[_currenIndex];
+            else return Storage[++_currenIndex];
+        }
+    }
+}
